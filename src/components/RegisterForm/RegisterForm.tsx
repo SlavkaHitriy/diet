c94029929bdd02bad25de3a8068fdbc4677a5559
SlavkaHitriy <slavik.nukutuk@gmail.com>
2023-10-23 03:44:23 +0300

    Added api request method, error handling, axios

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,64 +1,72 @@
-import { component$, useSignal, $ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-import { routeAction$, routeLoader$ } from '@builder.io/qwik-city';
-import { type InitialValues, useForm, SubmitHandler } from '@modular-forms/qwik';
-import { RegisterForm, useFormLoader } from "~/routes/auth/register";
-
-
+import { $, component$, useSignal } from '@builder.io/qwik';
+import { SubmitHandler, useForm } from '@modular-forms/qwik';
+import { RegisterForm, useFormLoader } from '~/routes/auth/register';
+import { registerUser } from '~/api/auth/register';
 
 type RegisterFormKeys = keyof RegisterForm;
 interface RegisterFormFields {
     fieldName: RegisterFormKeys;
-    type: "text" | "email" | "password";
+    type: 'text' | 'email' | 'password';
 }
-const formFields: RegisterFormFields[] = [{
-    fieldName: "name",
-    type: "text",
-}, {
-    fieldName: "secondName",
-    type: "text"
-}, {
-    fieldName: "email",
-    type: "email"
-}, {
-    fieldName: "password",
-    type: "password"
-}, {
-    fieldName: "repeatPassword",
-    type: "password"
-}
+const formFields: RegisterFormFields[] = [
+    {
+        fieldName: 'name',
+        type: 'text',
+    },
+    {
+        fieldName: 'secondName',
+        type: 'text',
+    },
+    {
+        fieldName: 'email',
+        type: 'email',
+    },
+    {
+        fieldName: 'password',
+        type: 'password',
+    },
+    {
+        fieldName: 'repeatPassword',
+        type: 'password',
+    },
 ];
-
-
-
-
-
 
 export default component$(() => {
     const [registerForm, { Form, Field, FieldArray }] = useForm<RegisterForm>({
         loader: useFormLoader(),
     });
 
+    const error = useSignal('');
 
-    const error = useSignal("");
+    const handleSubmit = $<SubmitHandler<RegisterForm>>(
+        async (values, event) => {
+            if (values.password === values.repeatPassword) {
+                const response = await registerUser(values);
 
-    const handleSubmit = $<SubmitHandler<RegisterForm>>((values, event) => {
-        if (values.password === values.repeatPassword) {
-            console.log(values);
-        } else {
-            error.value = "Паролі не співпадають";
-        }
-    });
+                if (response.isError) {
+                    error.value = response.error?.message ?? 'Error';
+                }
+            } else {
+                error.value = 'Паролі не співпадають';
+            }
+        },
+    );
+
     return (
         <Form onSubmit$={handleSubmit}>
-
             {formFields.map((fields) => (
                 <Field name={fields.fieldName} key={fields.fieldName}>
                     {(field, props) => {
                         return (
                             <>
-                                <label for={fields.fieldName}>{fields.fieldName}</label>
-                                <input {...props} type={fields.type} value={field.value} />
+                                <label for={fields.fieldName}>
+                                    {fields.fieldName}
+                                </label>
+                                <input
+                                    {...props}
+                                    type={fields.type}
+                                    value={field.value}
+                                />
                             </>
                         );
                     }}
@@ -68,9 +76,4 @@ export default component$(() => {
             {error.value && <div>{error.value}</div>}
         </Form>
     );
-
 });
-
-
-
-
--- src/routes/auth/register/index.tsx
+++ src/routes/auth/register/index.tsx
@@ -1,40 +1,35 @@
-import { component$, useSignal } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, useSignal } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
 import { routeLoader$ } from '@builder.io/qwik-city';
 import { type InitialValues, useForm } from '@modular-forms/qwik';
-import RegisterForm from "~/components/RegisterForm/RegisterForm";
-
+import RegisterForm from '~/components/RegisterForm/RegisterForm';
 
 export type RegisterForm = {
-  name: string;
-  secondName: string;
-  email: string;
-  password: string;
-  repeatPassword: string;
+    name: string;
+    secondName: string;
+    email: string;
+    password: string;
+    repeatPassword: string;
 };
 
-
 export const useFormLoader = routeLoader$<InitialValues<RegisterForm>>(() => ({
-  name: '',
-  secondName: '',
-  email: '',
-  password: '',
-  repeatPassword: '',
+    name: '',
+    secondName: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
 }));
 
 export default component$(() => {
-  return (
-    <RegisterForm />
-  );
+    return <RegisterForm />;
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to register",
-  meta: [
-    {
-      name: "register or mother die",
-      content: "mother car",
-    },
-  ],
-
+    title: 'Welcome to register',
+    meta: [
+        {
+            name: 'register or mother die',
+            content: 'mother car',
+        },
+    ],
 };
