0856f9d4ef8e80f8c57ad51abdbd79601e13506d
slavka <slavik.nukutuk@gmail.com>
2023-12-05 22:27:15 +0200

    Added db connection

--- src/components/ProfileContent/ProfileContent.tsx
+++ src/components/ProfileContent/ProfileContent.tsx
@@ -2,6 +2,19 @@ import { component$, useStore, useVisibleTask$ } from '@builder.io/qwik';
 import styles from './ProfileContent.module.scss';
 import { api } from '~/api';
 import Loader from '~/components/Loader';
+import { server$ } from '@builder.io/qwik-city';
+import sql from 'mssql/msnodesqlv8';
+import { config } from 'mssql';
+
+const sqlConfig: config = {
+    port: 1433,
+    database: 'VNTUDiet',
+    server: 'localhost\\SQLEXPRESS',
+    // driver: 'msnodesqlv8',
+    options: {
+        trustedConnection: true,
+    },
+};
 
 interface UseStoreData {
     data: UserData | null;
@@ -16,15 +29,43 @@ export default component$(() => {
         data: null,
     });
 
-    useVisibleTask$(async () => {
-        const response = await api.getUserDetails(
-            {},
-            { userId: localStorage.getItem('userId') || '' },
-        );
+    const getUserDetails = server$(async (id = '') => {
+        sql.connect(
+            `Data Source=localhost\\SQLEXPRESS;Initial Catalog=VNTUDiet;Integrated Security=True;TrustServerCertificate=True`,
+            (err) => {
+                if (err) {
+                    console.log(err);
+                }
 
-        if (response.data) {
-            userData.data = response.data;
-            console.log(response.data);
+                const request = new sql.Request();
+                request.query(
+                    `SELECT * FROM [AspNetUsers] WHERE Id = ${id}`,
+                    (err, result) => {
+                        if (err) {
+                            console.log('err', err);
+                        } else {
+                            // @ts-ignore
+                            userData.data = result!.recordset[0];
+                        }
+                    },
+                );
+            },
+        );
+    });
+
+    useVisibleTask$(async () => {
+        getUserDetails();
+
+        if (!userData.data) {
+            const response = await api.getUserDetails(
+                {},
+                { userId: localStorage.getItem('userId') || '' },
+            );
+
+            if (response.data) {
+                userData.data = response.data;
+                console.log(response.data);
+            }
         }
     });
 
